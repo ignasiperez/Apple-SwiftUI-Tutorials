aac3c4e8b026b2a0f918960d32fc062a13d7f0c4
Ignasi Perez <perezignasi@gmail.com>
2019-06-04 22:42:40 +0200

    Initial commit

---
+++ Landmarks/Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Ignasi Perez-Valls on 04/06/2019.
+//  Copyright © 2019 ignasiSwift. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
