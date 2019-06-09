960e3b003624a40b14794a32faaad109005a50a6
Ignasi Perez <perezignasi@gmail.com>
2019-06-09 08:20:39 +0200

    SwiftUI-Tutorial101 – Landmarks

--- SwiftUITutorial101/SwiftUITutorial101/CircleImage.swift
+++ SwiftUITutorial101/SwiftUITutorial101/CircleImage.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct CircleImage: View {
     var body: some View {
+        // S04-03 Replace the text view with the image of Turtle Rock by using the Image(_:) initializer.
         Image("turtlerock").clipShape(Circle())
             .overlay(Circle()
             .stroke(Color.gray, lineWidth: 4)
--- SwiftUITutorial101/SwiftUITutorial101/ContentView.swift
+++ SwiftUITutorial101/SwiftUITutorial101/ContentView.swift
@@ -8,27 +8,40 @@
 
 import SwiftUI
 
-// Conforms to the View protocol and describes the view’s content and layout.
+// S01-04 Conforms to the View protocol and describes the view’s content and layout.
 struct ContentView : View {
     var body: some View {
-        VStack {
+        
+        // S03-05 By default, stacks center their contents along their axis and provide context-appropriate spacing.
+        VStack(alignment: .leading) {
+//            S01-06
 //            Text("Hello SwiftUI!")
-            Text("Turtle Rock").font(.title)
+            
+            // S02-04
+            // To customize a SwiftUI view, you call methods called MODIFIERS.
+            // MODIFIERS wrap a view to change its display or other properties.
+            // EACH MODIFIER RETURNS A NEW VIEW, so it’s common to chain multiple modifiers, stacked vertically.
+            Text("Turtle Rock").font(.title).color(.green)
+            
             HStack {
                 Text("Joshua Tree National Park")
                     .font(.subheadline)
+                
+                // S03-08 To use the full width of the device, separate the park and the state by adding a Spacer to the horizontal stack.
                 Spacer()
+                
                 Text("California").font(.subheadline)
             }
         }
-            
+        
+        // S03-09 Use the padding() modifier method to give the landmark’s name and details a little breathing room.
         .padding()
     }
 }
 
 #if DEBUG
 
-// Declares a preview of the ContentView struct.
+// S01-04 Declares a preview of the ContentView struct.
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
         ContentView()
