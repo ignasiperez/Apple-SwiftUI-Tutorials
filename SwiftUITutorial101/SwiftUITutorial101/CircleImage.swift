f42483fca225bfbcb5ebc2c9f1687401510d5547
Ignasi Perez <perezignasi@gmail.com>
2019-06-10 23:51:25 +0200

    SwiftUI-Tutorial101 – Landmarks

--- SwiftUITutorial101/SwiftUITutorial101/CircleImage.swift
+++ SwiftUITutorial101/SwiftUITutorial101/CircleImage.swift
@@ -11,14 +11,30 @@ import SwiftUI
 struct CircleImage: View {
     var body: some View {
         // S04-03 Replace the text view with the image of Turtle Rock by using the Image(_:) initializer.
+        //
+        // S04-04 Add a call to .clipShape(Circle()) to apply the circular clipping shape to the image. The Circle type is a shape that you can use as a mask, or as a view by giving the circle a stroke or fill.
+        //
+        // S04-05 Create another circle with a gray stroke, and then add it as an overlay to give the image a border.
+        //
+        // S04-06 Add a shadow with a 10 point radius.
         Image("turtlerock").clipShape(Circle())
             .overlay(Circle()
-            .stroke(Color.gray, lineWidth: 4)
+            .stroke(Color.white, lineWidth: 4)
         )
-            
         .shadow(radius: 10)
     }
 }
+
+/*
+ var body: some View {
+ Image("turtlerock")
+ .clipShape(Circle())
+ .overlay(
+ Circle().stroke(Color.white, lineWidth: 4))
+ .shadow(radius: 10)
+ }
+ */
+
 
 #if DEBUG
 struct CircleImage_Previews : PreviewProvider {
---
+++ SwiftUITutorial101/SwiftUITutorial101/MapView.swift
@@ -0,0 +1,24 @@
+//
+//  MapView.swift
+//  SwiftUITutorial101
+//
+//  Created by Ignasi Perez-Valls on 10/06/2019.
+//  Copyright © 2019 ignasiSwift. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView : UIViewRepresentable {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
